1cee513721e3edbf4469b1fe6e4c001d4968b0da
aladino24 <aladinozulmar@gmail.com>
2023-04-09 13:46:37 +0700

    notif dialog

--- lib/widgets/main_header.dart
+++ lib/widgets/main_header.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:badges/badges.dart' as badge;
 import 'package:kharisma_sales_app/controllers/components/main_header_controller.dart';
+import 'package:kharisma_sales_app/widgets/notification_dialog.dart';
 import 'package:kharisma_sales_app/routes/routes_name.dart';
 import 'package:kharisma_sales_app/constants/apps_colors.dart';
 import 'package:get/get.dart';
@@ -122,142 +123,8 @@ class MainHeader extends StatelessWidget {
               onTap: () {
                 controller.selectNotificationIcon();
                 // Get.toNamed(RoutesName.notificationProduct);
-                showDialog(
-                  context: context,
-                  builder: (BuildContext context) {
-                    return Container(
-                      margin: EdgeInsets.only(bottom: 200),
-                      padding: const EdgeInsets.symmetric(horizontal: 15),
-                      child: AlertDialog(
-                        scrollable: true,
-                        insetPadding: EdgeInsets.zero,
-                        content: Container(
-                          width: 350.0,
-                          height: 300,
-                          child: SingleChildScrollView(
-                            child: Column(
-                              mainAxisSize: MainAxisSize.min,
-                              children: [
-                                Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceBetween,
-                                  children: [
-                                    Text(
-                                      'Notification',
-                                      style: TextStyle(
-                                          fontSize: 18,
-                                          fontWeight: FontWeight.bold),
-                                    ),
-                                    Text(
-                                      'Mark All as Read',
-                                      style: TextStyle(
-                                          color: AppsColors.loginColorPrimary,
-                                          fontSize: 10.0),
-                                    )
-                                  ],
-                                ),
-                                Divider(
-                                  color: Colors.grey,
-                                ),
-                                Container(
-                                  padding: EdgeInsets.all(8),
-                                  decoration: BoxDecoration(
-                                    // color with opacity
-                                    color: AppsColors.loginColorPrimary
-                                        .withOpacity(0.2),
-                                  ),
-                                  child: Column(
-                                    children: [
-                                      Row(
-                                        children: [
-                                          CircleAvatar(
-                                            radius: 20,
-                                            backgroundColor:
-                                                AppsColors.loginColorPrimary,
-                                            child: Icon(
-                                              Icons.notifications,
-                                              color: Colors.white,
-                                            ),
-                                          ),
-                                          SizedBox(width: 10),
-                                          Expanded(
-                                            child: Column(
-                                              children: [
-                                                Text(
-                                                  'Terimakasih sudah melakukan pembayaran transaksi INV-2023/2403000024 sejumlah 54.300',
-                                                  style: TextStyle(
-                                                    fontSize: 14,
-                                                  ),
-                                                ),
-                                                Text(
-                                                  'Tunggu sebentar, pembayaranmu sedang kami proses..',
-                                                  style: TextStyle(
-                                                      fontSize: 12,
-                                                      color: AppsColors
-                                                          .loginFontColorSecondary),
-                                                ),
-                                              ],
-                                            ),
-                                          ),
-                                        ],
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                                Divider(
-                                  color: Colors.grey,
-                                ),
-                                Container(
-                                  padding: EdgeInsets.all(8),
-                                  // decoration: BoxDecoration(
-                                  //   // color with opacity
-                                  //   color: AppsColors.loginColorPrimary.withOpacity(0.2),
-                                  // ),
-                                  child: Column(
-                                    children: [
-                                      Row(
-                                        children: [
-                                          CircleAvatar(
-                                            radius: 20,
-                                            backgroundColor:
-                                                AppsColors.loginColorPrimary,
-                                            child: Icon(
-                                              Icons.notifications,
-                                              color: Colors.white,
-                                            ),
-                                          ),
-                                          SizedBox(width: 10),
-                                          Expanded(
-                                            child: Column(
-                                              children: [
-                                                Text(
-                                                  'Terimakasih sudah melakukan pembayaran transaksi INV-2023/2403000024 sejumlah 54.300',
-                                                  style: TextStyle(
-                                                    fontSize: 14,
-                                                  ),
-                                                ),
-                                                Text(
-                                                  'Tunggu sebentar, pembayaranmu sedang kami proses..',
-                                                  style: TextStyle(
-                                                      fontSize: 12,
-                                                      color: AppsColors
-                                                          .loginFontColorSecondary),
-                                                ),
-                                              ],
-                                            ),
-                                          ),
-                                        ],
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
-                      ),
-                    );
-                  },
+                Get.dialog(
+                  NotificationDialog(),
                 );
               },
               child: badge.Badge(
@@ -307,3 +174,5 @@ class MainHeader extends StatelessWidget {
     });
   }
 }
+
+
